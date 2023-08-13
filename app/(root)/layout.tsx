f15daf655e8df612e71e4d4c0796163461ddb9c0
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2023-08-13 21:13:01 +0530

    added basic structure, setup clerk-auth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+
+import { Metadata } from "next";
+import { Inter } from 'next/font/google'
+
+import clsx from "clsx"
+import { dark } from "@clerk/themes";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import '../globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next Social',
+  description: 'Threads Clone built with Next.js',
+}
+
+function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
+      <html lang="en">
+        <body className={clsx(inter.className, 'bg-dark-1')}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
+
+export default RootLayout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+function Home() {
+
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+
+  )
+
+}
+
+export default Home;
